a9ab7a51cc66d871bbccf860543e8611c169bdba
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-09-13 20:50:50 +0200

    Script pulls S&P monthly returns from the Ycharts website

---
+++ Scrape Montly S&P500 data.R
@@ -0,0 +1,13 @@
+library(httr)
+library(rvest)
+library(dplyr)
+library(stringr)
+
+response <- GET('https://ycharts.com/indicators/sp_500_monthly_return')
+sp <- content(response)
+
+sp %>%
+  html_nodes(xpath = '//table/tbody//tr//td[@class = "text-right"]') %>%
+  html_text() %>%
+  str_extract("[0-9]+\\.[0-9]+")
+  
