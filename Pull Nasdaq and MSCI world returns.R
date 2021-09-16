49d8f2eab1c9b6bb00a822a84ec41079f6404fa0
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-09-16 20:11:52 +0200

    Constructing and cleaning xts object

---
+++ Pull Nasdaq and MSCI world returns.R
@@ -0,0 +1,27 @@
+library(quantmod)
+library(xts)
+library(readxl)
+
+nasdaq <- getSymbols(Symbols = "^IXIC", src = "yahoo", auto.assign = FALSE)
+world <- getSymbols(Symbols = "URTH", src = "yahoo", auto.assign = FALSE)
+sp <- getSymbols(Symbols = "^GSPC", src = "yahoo", auto.assign = FALSE)
+
+nasdaq <- to.period(nasdaq, period = "months", k = 1, OHLC = FALSE)
+world <- to.period(world, period = "months", k = 1, OHLC = FALSE)
+sp <- to.period(sp, period = "months", k = 1, OHLC = FALSE)
+
+nasdaq <- nasdaq["2013-07/2021-08"]
+world <- world["2013-07/2021-08"]
+sp <- sp["2013-07/2021-08"]
+
+idx <- index(nasdaq)
+
+etorocopy <- read_excel("Etoro.xlsx", col_names = TRUE)
+etorocopy <- xts(etorocopy, order.by = idx)
+
+nasdaq$nasdaq <- monthlyReturn(nasdaq)*100
+world$world <- monthlyReturn(world)*100
+sp$sp <- monthlyReturn(sp)*100
+
+etorocopy <- merge(etorocopy, nasdaq$nasdaq, world$world, sp$sp, join = "left")
+etorocopy <- etorocopy[-1]
