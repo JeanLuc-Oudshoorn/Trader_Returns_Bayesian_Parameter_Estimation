5f5d2363ac8ae77e315d1306fd9448b173b7f1f2
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-09-17 15:08:34 +0200

    The uncertainty of the mean and standard deviation of the returns of each trader is now propogated into the random samples used to construct the density function.
    Additionally, a metrics dataframe is generated that displays sample quantiles along with the expected percentage of months that returns will be positive.

--- T distribution fit.R
+++ T distribution fit.R
@@ -1,7 +1,10 @@
 library(dplyr)
+library(QRM)
 
 fit <- data.frame(nu = rep(0, 18), mu = rep(0, 18), sigma = rep(0,18))
+ses <- data.frame(nus = rep(0, 18), mus = rep(0, 18), sigmas = rep(0,18))
 output <- c()
+
 
 for (i in 1:ncol(etorocopy)){
     if(i == 4 | i == 14){
@@ -9,11 +12,16 @@ for (i in 1:ncol(etorocopy)){
     }
     output <- fit.st(na.omit(etorocopy[,i]))
     print(output$par.ests)
+    print(output$par.ses)
     fit[i,] <- output$par.ests
+    ses[i,] <- output$par.ses
 }
 
 names <- colnames(etorocopy)
 rownames(fit) <- names
+rownames(ses) <- names
+
+fit <- cbind(fit, ses)
 
 attach(fit)
 ranking <- fit[order(-nu),]
@@ -22,17 +30,23 @@ print(ranking)
 samples <- matrix(nrow = 10000, ncol = 18)
 
 for(i in 1:nrow(ranking)){
- samples[,i] <- (rt(10000, df = ranking$nu[i])*ranking$sigma[i] + ranking$mu[i])
+ samples[,i] <- (rt(10000, df = ranking$nu[i]) * rnorm(10000, ranking$sigma[i], ranking$sigmas[i]) + 
+                   rnorm(10000, ranking$mu[i], ranking$mus[i]))
 }
 colnames(samples) <- rownames(ranking)
 
-exp_pos <- matrix(nrow = 18)
+metrics <- as.data.frame(matrix(nrow = 18, ncol = 8))
+rownames(metrics) <- colnames(samples)
+colnames(metrics) <- c("99%", "VaR", "70%", "50%", "20%", "5%", "1%", "ExpPos")
 
 for (i in 1:16){
-  exp_pos[i,] <- sum(samples[,i] > 0)/length(samples[,i])
+  metrics[i,] <- quantile(samples[,i], c(0.01, 0.05, 0.3, 0.5, 0.8, 0.95, 0.99, 1))
+  metrics$ExpPos[i] <- sum(samples[,i] > 0)/length(samples[,i])*100
 }
 
-rownames(exp_pos) <- colnames(samples)
+metrics <- round(metrics[1:16,], 1)
+
+samples <- as.data.frame(samples)
 
 long_samples <- pivot_longer(samples, cols = 1:16, names_to = "trader", values_to = "return")
 long_samples <- long_samples[,3:4]
@@ -43,7 +57,7 @@ long_samples[sub_1,]
   ggplot(long_samples[sub_1,], aes(x = return, color = trader)) +
   geom_density() +
   xlim(-15, 15) +
-  ylim(0, 0.175)
+  ylim(0, 0.2)
   
 sub_2 <- long_samples$trader %in% unique(long_samples$trader)[6:10]
 long_samples[sub_2,]
@@ -51,7 +65,7 @@ long_samples[sub_2,]
   ggplot(long_samples[sub_2,], aes(x = return, color = trader)) +
     geom_density() +
     xlim(-15, 15) +
-    ylim(0, 0.175)
+    ylim(0, 0.2)
   
 sub_3 <- long_samples$trader %in% unique(long_samples$trader)[11:16]
 long_samples[sub_3,]
@@ -59,5 +73,5 @@ long_samples[sub_3,]
   ggplot(long_samples[sub_3,], aes(x = return, color = trader)) +
     geom_density() +
     xlim(-15, 15) +
-    ylim(0, 0.175)
+    ylim(0, 0.2)
   
