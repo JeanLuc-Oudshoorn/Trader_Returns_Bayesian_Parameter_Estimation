3e4d4f0a294bd737d150291155e788fc2c2a2b85
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-10-25 16:33:41 +0200

    Fixed a bug with the gain column. Also updated the formula to account for inflation more accurately.

--- Wealth.R
+++ Wealth.R
@@ -1,7 +1,7 @@
 wealth <- function(principal = 53000, return = 1.20, inflation = 0.02, years = 20){
     df <- data.frame(year = 1:years, value = rep(0, years))
     for (i in 1:years){
-      amount <- principal * (return - inflation)^i
+      amount <- principal * (return * (1-inflation))^i
           if(amount <= 50000){
             df$value[i] <- amount
           } else if(amount <= 100000){
@@ -12,12 +12,11 @@ wealth <- function(principal = 53000, return = 1.20, inflation = 0.02, years = 2
             df$value[i] <- amount - (50000*0.01898*0.31) - (900000*0.04501*0.31) - (amount - 1000000)*0.0569*0.31
           }
       }
-    for(i in 1:nrow(df)){
-    df$gain[i] <- df$value[i]-df$value[(i-1)]
-    }
+    gain <- diff(df$value)   
+    df$gain <- c(NA, gain)
     print(round(df, 0))
     print(paste("Inflation-adjusted millionaire after", min(which(df$value > 1000000)), "years!"))
-    print(paste("You can safely stop working after", min(which(df$gain > 100000)), "years!"))
+    print(paste("You can safely stop working after", min(which(gain > 100000))+1, "years!"))
     plot(df$value, type = "l", col = "goldenrod")
     abline(h = 1000000, col = "lightgray")
     
