d7b3eddb7345d6c3d996e7a7ec14e8ce3ed2bf1b
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-11-04 19:37:22 +0100

    Added comments and fixed the script to use log-returns.

--- Bayesian Outperformance Estimation.R
+++ Bayesian Outperformance Estimation.R
@@ -4,8 +4,9 @@ library(dplyr)
 bayes_fit <- list()
 bayes_fitsp <- list()
 
+## 1.) Estimate outperformance of log returns vs NASDAQ
 for(i in 1:15){
-    df <- as.data.frame(etorocopy) %>%
+    df <- as.data.frame(popreturn) %>%
       select(i, nasdaq) %>%
       na.omit() 
   
@@ -16,15 +17,17 @@ for(i in 1:15){
   bayes_fit[[i]] <- BESTout
 }
 
-traders <- colnames(etorocopy)
+## 2.) Set up a plotting grid and generate the probability distributions
+traders <- colnames(popreturn)
 par(mfrow = c(2, 2))
 
 for(i in 1:15){
    plot(bayes_fit[[i]], main = paste(traders[i], "performance vs. nasdaq"))
   }
 
+## 3.) Estimate outperformance of log returns vs S&P500
 for(i in 1:15){
-  df <- as.data.frame(etorocopy) %>%
+  df <- as.data.frame(popreturn) %>%
     select(i, sp) %>%
     na.omit() 
   
@@ -35,11 +38,29 @@ for(i in 1:15){
   bayes_fitsp[[i]] <- BESTout
 }
 
+## 4.) Plot outperformance against S&P500
 for(i in 1:15){
   plot(bayes_fitsp[[i]], main = paste(traders[i], "performance vs. S&P 500"))
 }
 
-detorocopy %>%
-  select(1, 2, 3, 5, 10, 12, 15:18) %>%
-  na.omit() %>%
-  cor()
+
+## 5.) Create a correlation matrix of returns of popular investors and those of indices
+cormatrix <- matrix(nrow = 15, ncol = 2) 
+
+for(i in 1:15){
+  correl <- as.data.frame(popreturn) %>%
+                  select(i, nasdaq) %>%
+                  na.omit() %>%
+                  cor()
+cormatrix[i, 1] <- correl[1,2]
+  
+  correl <- as.data.frame(popreturn) %>%
+                  select(i, sp) %>%
+                  na.omit() %>%
+                  cor()
+cormatrix[i, 2] <- correl[1,2]
+}
+
+colnames(cormatrix) <- c("nasdaq", "s&p500")
+rownames(cormatrix) <- c("jeppe", "mariano", "victor", "jurgen", "reinhardt", "martina", "wesley", "heloise",
+                         "kieran", "harry", "richard", "lena", "eddy", "teoh", "libor")
