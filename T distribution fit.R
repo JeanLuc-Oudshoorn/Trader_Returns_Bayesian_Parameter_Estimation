037580243ffeb75fba67b85bba146dac1a2f2784
JeanLuc-Oudshoorn <jeanluc@oudshoorn.pro>
2021-11-04 17:04:59 +0100

    Updated the script to use a scaled t-distribution that results in better fit. Also adapted the sampling method to use the log-normal distribution where this was more appropriate.

--- T distribution fit.R
+++ T distribution fit.R
@@ -1,77 +1,97 @@
 library(dplyr)
-library(QRM)
+library(tidyr)
+library(xts)
+library(fitdistrplus)
+library(metRology)
 
-fit <- data.frame(nu = rep(0, 18), mu = rep(0, 18), sigma = rep(0,18))
-ses <- data.frame(nus = rep(0, 18), mus = rep(0, 18), sigmas = rep(0,18))
+fit <- data.frame(df = rep(0, 17), mean = rep(0, 17), sd = rep(0,17))
+ses <- data.frame(dfs = rep(0, 17), means = rep(0, 17), sds = rep(0,17))
 output <- c()
 
 
-for (i in 1:ncol(etorocopy)){
-    if(i == 4 | i == 14){
-      next
-    }
-    output <- fit.st(na.omit(etorocopy[,i]))
-    print(output$par.ests)
-    print(output$par.ses)
-    fit[i,] <- output$par.ests
-    ses[i,] <- output$par.ses
+for (i in 1:ncol(popreturn)){
+  
+  output <- fitdist(as.vector(na.omit(coredata(popreturn[,i]))), "t.scaled",
+                    start = list(df = 3,
+                                 mean = mean(na.omit(coredata(popreturn[,i]))),
+                                 sd = sd(na.omit(coredata(popreturn[,i]))))) 
+  print(output)
+  fit[i,] <- output$estimate
+  ses[i,] <- output$sd
+ 
+
+ if(fit$df[i]>100){
+      output <- fitdist(as.vector(na.omit(coredata(popreturn[,i]))), "norm")
+      print(output)
+      fit$mean[i] <- output$estimate[1]
+      fit$sd[i] <- output$estimate[2]
+      ses$means[i] <- output$sd[1]
+      ses$sds[i] <- output$sd[2]
+  }
 }
 
-names <- colnames(etorocopy)
+
+names <- colnames(popreturn)
 rownames(fit) <- names
 rownames(ses) <- names
 
 fit <- cbind(fit, ses)
 
-attach(fit)
-ranking <- fit[order(-nu),]
-print(ranking)
 
-samples <- matrix(nrow = 10000, ncol = 18)
+samples <- matrix(nrow = 10000, ncol = 17)
 
-for(i in 1:nrow(ranking)){
- samples[,i] <- (rt(10000, df = ranking$nu[i]) * rnorm(10000, ranking$sigma[i], ranking$sigmas[i]) + 
-                   rnorm(10000, ranking$mu[i], ranking$mus[i]))
-}
-colnames(samples) <- rownames(ranking)
+for(i in 1:nrow(fit)){
+  if(fit$df[i] < 100){
+    
+    samples[,i] <-  (rt(10000, df =  fit$df[i]) *
+                        rnorm(10000, fit$sd[i], fit$sds[i]) + 
+                        rnorm(10000, fit$mean[i], fit$means[i]))
+  } else {
+        samples[,i] <- (rnorm(10000, mean = rnorm(10000, fit$mean[i], fit$means[i]), sd = rnorm(10000, fit$sd[i], fit$sds[i])))
+  }
+}  
 
-metrics <- as.data.frame(matrix(nrow = 18, ncol = 8))
+
+colnames(samples) <- rownames(fit)
+
+metrics <- as.data.frame(matrix(nrow = 17, ncol = 8))
 rownames(metrics) <- colnames(samples)
 colnames(metrics) <- c("99%", "VaR", "70%", "50%", "20%", "5%", "1%", "ExpPos")
 
-for (i in 1:16){
+for (i in 1:17){
   metrics[i,] <- quantile(samples[,i], c(0.01, 0.05, 0.3, 0.5, 0.8, 0.95, 0.99, 1))
   metrics$ExpPos[i] <- sum(samples[,i] > 0)/length(samples[,i])*100
 }
 
-metrics <- round(metrics[1:16,], 1)
+metrics <- round(metrics[1:17,], 3)
 
 samples <- as.data.frame(samples)
 
-long_samples <- pivot_longer(samples, cols = 1:16, names_to = "trader", values_to = "return")
-long_samples <- long_samples[,3:4]
+long_samples <- pivot_longer(samples, cols = 1:17, names_to = "trader", values_to = "return")
 
-sub_1 <- long_samples$trader %in% unique(long_samples$trader)[1:5]
+sub_1 <- long_samples$trader %in% unique(long_samples$trader)[1:6]
 long_samples[sub_1,]
 
   ggplot(long_samples[sub_1,], aes(x = return, color = trader)) +
   geom_density() +
-  xlim(-15, 15) +
-  ylim(0, 0.2)
+  xlim(-0.2, 0.2) +
+  ylim(0, 21)
   
-sub_2 <- long_samples$trader %in% unique(long_samples$trader)[6:10]
+sub_2 <- long_samples$trader %in% unique(long_samples$trader)[7:11]
 long_samples[sub_2,]
   
   ggplot(long_samples[sub_2,], aes(x = return, color = trader)) +
     geom_density() +
-    xlim(-15, 15) +
-    ylim(0, 0.2)
+    xlim(-0.2, 0.2) +
+    ylim(0, 21)
   
-sub_3 <- long_samples$trader %in% unique(long_samples$trader)[11:16]
+sub_3 <- long_samples$trader %in% unique(long_samples$trader)[12:17]
 long_samples[sub_3,]
   
   ggplot(long_samples[sub_3,], aes(x = return, color = trader)) +
     geom_density() +
-    xlim(-15, 15) +
-    ylim(0, 0.2)
+    xlim(-0.2, 0.2) +
+    ylim(0, 21)
+
+  
   
